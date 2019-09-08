580b909464e49a922e56a9faa17d261bba73651b
Kacper <kapceru@gmail.com>
2019-09-08 14:20:17 +0200

    dodano pokoje

--- src/main/java/com/bookingproject/bookingproject/service/RoomManager.java
+++ src/main/java/com/bookingproject/bookingproject/service/RoomManager.java
@@ -18,10 +18,80 @@ public class RoomManager implements InitializingBean {
         this.roomRepository = roomRepository;
     }
 
-    public void saveRoom() {
+    public void saveRoom1() {
         Room room = new Room();
         room.setRoomCategory(RoomCategory.DELUXE);
-        room.setPrice(BigDecimal.valueOf(299.99));
+        room.setPrice(BigDecimal.valueOf(999));
+        room.setBeds("awesowe");
+        room.setRoomStatus(RoomStatus.CLEAN);
+        roomRepository.save(room);
+    }public void saveRoom2() {
+        Room room = new Room();
+        room.setRoomCategory(RoomCategory.DELUXE);
+        room.setPrice(BigDecimal.valueOf(999));
+        room.setBeds("tigers skin");
+        room.setRoomStatus(RoomStatus.CLEAN);
+        roomRepository.save(room);
+    }public void saveRoom3() {
+        Room room = new Room();
+        room.setRoomCategory(RoomCategory.DELUXE);
+        room.setPrice(BigDecimal.valueOf(899));
+        room.setBeds("snakeskin");
+        room.setRoomStatus(RoomStatus.CLEAN);
+        roomRepository.save(room);
+    }public void saveRoom4() {
+        Room room = new Room();
+        room.setRoomCategory(RoomCategory.EXECUTIVE);
+        room.setPrice(BigDecimal.valueOf(9999));
+        room.setBeds("damamged");
+        room.setRoomStatus(RoomStatus.DAMAGED);
+        roomRepository.save(room);
+    }public void saveRoom5() {
+        Room room = new Room();
+        room.setRoomCategory(RoomCategory.STUDIO);
+        room.setPrice(BigDecimal.valueOf(500));
+        room.setBeds("double");
+        room.setRoomStatus(RoomStatus.CLEAN);
+        roomRepository.save(room);
+    }public void saveRoom6() {
+        Room room = new Room();
+        room.setRoomCategory(RoomCategory.SUITE);
+        room.setPrice(BigDecimal.valueOf(150));
+        room.setBeds("king");
+        room.setRoomStatus(RoomStatus.CLEAN);
+        roomRepository.save(room);
+    }public void saveRoom7() {
+        Room room = new Room();
+        room.setRoomCategory(RoomCategory.SUITE);
+        room.setPrice(BigDecimal.valueOf(170));
+        room.setBeds("king");
+        room.setRoomStatus(RoomStatus.CLEAN);
+        roomRepository.save(room);
+    }public void saveRoom8() {
+        Room room = new Room();
+        room.setRoomCategory(RoomCategory.STUDIO);
+        room.setPrice(BigDecimal.valueOf(100));
+        room.setBeds("double");
+        room.setRoomStatus(RoomStatus.DIRTY);
+        roomRepository.save(room);
+    }public void saveRoom9() {
+        Room room = new Room();
+        room.setRoomCategory(RoomCategory.SUITE);
+        room.setPrice(BigDecimal.valueOf(200));
+        room.setBeds("single");
+        room.setRoomStatus(RoomStatus.CLEAN);
+        roomRepository.save(room);
+    }public void saveRoom10() {
+        Room room = new Room();
+        room.setRoomCategory(RoomCategory.SUITE);
+        room.setPrice(BigDecimal.valueOf(80));
+        room.setBeds("single");
+        room.setRoomStatus(RoomStatus.DIRTY);
+        roomRepository.save(room);
+    }public void saveRoom11() {
+        Room room = new Room();
+        room.setRoomCategory(RoomCategory.SUITE);
+        room.setPrice(BigDecimal.valueOf(80));
         room.setBeds("king");
         room.setRoomStatus(RoomStatus.CLEAN);
         roomRepository.save(room);
@@ -29,7 +99,17 @@ public class RoomManager implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        saveRoom();
+        saveRoom1();
+        saveRoom2();
+        saveRoom3();
+        saveRoom4();
+        saveRoom5();
+        saveRoom6();
+        saveRoom7();
+        saveRoom8();
+        saveRoom9();
+        saveRoom10();
+        saveRoom11();
     }
 
     public List<Room> getRooms() {
